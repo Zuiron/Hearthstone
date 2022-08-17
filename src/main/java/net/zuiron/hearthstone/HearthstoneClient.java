a62476f20eae0843044963b03baf99e8efb80479
Zuiron <martinfloden90@gmail.com>
2022-08-17 12:26:57 +0200

    init

---
+++ src/main/java/net/zuiron/hearthstone/HearthstoneClient.java
@@ -0,0 +1,11 @@
+package net.zuiron.hearthstone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HearthstoneClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
